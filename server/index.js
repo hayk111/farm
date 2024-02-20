1a78710fb13f2ca13de379762cecc115299ec432
hayk111 <hayk.atoyan111@gmail.com>
2024-02-20 15:42:42 +0400

    Adds API implementation to the backend app

--- server/index.js
+++ server/index.js
@@ -1,8 +1,37 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-app.use('/', (req, res) => {
-    res.send('Server is running!!!');
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+let animals = [];
+
+app.get('/api/animals', (req, res) => {
+  res.json(animals);
 });
 
-app.listen(5000, console.log('Server started on port 5000.'));
+app.post('/api/animals', (req, res) => {
+  const newAnimal = req.body;
+
+  if (animals.some(animal => animal.name === newAnimal.name)) {
+    return res.status(400).json({ error: 'Animal with this name already exists' });
+  }
+
+  animals.push(newAnimal);
+  res.json(animals);
+});
+
+app.delete('/api/animals/:name', (req, res) => {
+  const animalNameToRemove = req.params.name;
+
+  animals = animals.filter(animal => animal.name !== animalNameToRemove);
+  res.json(animals);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
